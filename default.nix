3e377a6f4d413ffb431dffebca1195cf0eb82e54
Sébastien Maret <sebastien.maret@icloud.com>
2022-03-24 12:23:15 +0100

    pythonPackages.pvextractor: do not build
    This package fails to build because it depends on aplpy, which is
    broken in nixpkgs.

--- default.nix
+++ default.nix
@@ -10,7 +10,8 @@
       pythonPackages = pkgs.python3Packages;
     }
   );
-  pvextractor = pkgs.python3Packages.callPackage ./pkgs/python-modules/pvextractor { };
+  # FIXME: This fails to build because aplpy in broken in nixpkgs
+  #pvextractor = pkgs.python3Packages.callPackage ./pkgs/python-modules/pvextractor { };
   # FIXME: This fails to build because astroquery is broken in nixpkgs
   #pyradex = pkgs.python3Packages.callPackage ./pkgs/python-modules/pyradex { };
 }
