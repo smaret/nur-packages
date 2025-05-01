ee8521abde7acb26c0440717dc3c36caff8a4e22
Sébastien Maret <sebastien.maret@icloud.com>
2025-05-01 15:11:52 +0200

    pvextractor: fix build, 0.3 -> 0.4

--- pkgs/python-modules/pvextractor/default.nix
+++ pkgs/python-modules/pvextractor/default.nix
@@ -1,31 +1,55 @@
-{ lib
-, fetchPypi
-, buildPythonPackage
-, astropy
-, matplotlib
-, scipy
-, qtpy
-, spectral-cube
-, pytestCheckHook
-, pytest-astropy
-, setuptools_scm
+{
+  lib,
+  fetchPypi,
+  fetchpatch2,
+  buildPythonPackage,
+  astropy,
+  matplotlib,
+  scipy,
+  qtpy,
+  spectral-cube,
+  pytestCheckHook,
+  pytest-astropy,
+  setuptools,
+  setuptools_scm,
 }:
 
 buildPythonPackage rec {
   pname = "pvextractor";
-  version = "0.3";
-  format = "setuptools";
+  version = "0.4";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-pU6YZ2DO9rGG4C0cPZjX4cGOTnXF0LcTQ7X/Wf2cCcM=";
+    sha256 = "sha256-7WoRWTXoPm+qdycQfpYbxJ15FP7IXjMm5a32XN4eg84=";
   };
 
-  propagatedBuildInputs = [ astropy matplotlib scipy qtpy spectral-cube ];
+  patches = [
+    (fetchpatch2 {
+      # Fix distutils deprecation on Python >= 3.12
+      # See https://github.com/radio-astro-tools/pvextractor/issues/129
+      url = "https://github.com/radio-astro-tools/pvextractor/pull/129/commits/2191f5fd557cea80448a0beeb7e3fd0a12ab8ed2.patch";
+      hash = "sha256-V4rxNnN3YQPjUo8TWhTS6xMqajOJwd0o/1VOocypU1A=";
+    })
+  ];
 
-  nativeBuildInputs = [ setuptools_scm ];
+  build-system = [
+    setuptools
+    setuptools_scm
+  ];
 
-  checkInputs = [ pytestCheckHook pytest-astropy ];
+  dependencies = [
+    astropy
+    matplotlib
+    scipy
+    qtpy
+    spectral-cube
+  ];
+
+  checkInputs = [
+    pytestCheckHook
+    pytest-astropy
+  ];
 
   disabledTests = [
     # segfaults with Matplotlib 3.1 and later
@@ -33,12 +57,11 @@ buildPythonPackage rec {
   ];
 
   meta = {
+    changelog = "https://github.com/radio-astro-tools/pvextractor/releases/tag/${version}";
     description = "Position-Velocity Diagram Extractor";
-    homepage = http://radio-astro-tools.github.io;
+    homepage = "https://pvextractor.readthedocs.io";
     license = lib.licenses.bsd3;
     platforms = lib.platforms.all;
     maintainers = with lib.maintainers; [ smaret ];
-    broken = true;
   };
 }
-
